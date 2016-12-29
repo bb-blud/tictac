46c2a47c4a358e88e748622f612969a34a14f33f
bb-blud <rsalo001@gmail.com>
2016-12-29 11:27:52 -0500

    Implemented the stateMeasure function, have yet to test it. I realized also that counting "lines" that have just one mark on them is important, since they also measure the value of the position. For example having the central square without any other marks yeilds 4 points for X, since there are 4 potential lines that can be developed from it, central vertical, central horizontal, and the two diagonals. On the other hand, a side square will potentially only develop into either a vertical or horizontal line. Corners are in between these two cases.

--- strateegery.py
+++ strateegery.py
@@ -7,8 +7,6 @@ class Strateegery(object):
     def measureState(self):
         gs = self.game_state
         size = self.game_state.size
-        X_weight = 0
-        O_weight = 0
         state_weight = 0
         for direction in gs.lines:
 
@@ -19,13 +17,10 @@ class Strateegery(object):
             viables = (coord for coord in keys if  gs.lines[direction].get(coord, "Empty") not in ["Empty", None])
             
             for line in (gs.lines[direction][coord] for coord in viables):
-                if line[0] == 'X' and len(line[1:]) > 1:
-                    X_weight += len(line[1:])
-                if line[0] == 'O' and len(line[1:]) > 1:
-                    O_weight += len(line[1:])
-        
-        print "X_weight: ", X_weight
-        print "O_weight: ", O_weight
-        print
+                if line[0] == 'X':
+                    state_weight += len(line[1:])
+                if line[0] == 'O':
+                    state_weight -= len(line[1:])
                 
-        return X_weight, "working on it"
+        return state_weight
+
