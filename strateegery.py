488e638b3f2667d35a90447eca22198481f3cd08
bb-blud <rsalo001@gmail.com>
2017-01-14 13:48:50 -0500

    So I'm seeing somewhat encouraging results as I have been able to get the two reinforcement agents to start tieing each other after a few dozen games. The first main error I corrected was in the definition of charted moves in the reinforcement method inside strateegery. Basically I had looked only at the 'seen' indices at a current step and ignored the game_sequence history up to that point, so the alternating minimizing/maximizing was not functioning properly. Secondly I had awarded I gigantic sgn * size**4 reward to the final victory move of a sequence, this was causing the agents to move towards minimal length games where basically first player would always win. I separated the iterative looping game playing into a its own function where I can pass a QMap instance and strategy policies. This way I have the agents play randomly for 20 games or so, so that dictionary keys (game sequences) are visited in somewhat even spread and Q gets a nice broad and I guess technically _uniform_ initial population of keys. Then I recover that QMap and pass through 100 more games where both players are using reinforcement. Well before the 100 games are played (TODO: Determine that moment), the agents have thus far been converging to a single game that ends in a tie. It is not always the same one, as the process starts with that random initial "seeding". The games I am observing so far are very reasonable, in the sense that no moves are particularly bad; however, they are also very passive games, that is, the agents become adept at blocking possible wins and making honest attempts at making 3 marks in line, but no forking behavior seems to develop yet. I've only tried games of size 3. Next I will store (pickle) trained QMaps and start playing against the perfect player strategies. TODO: Add logic to turn learning on and off. That is, to be able to play just by reading from Q without updating it if desired.

--- strateegery.py
+++ strateegery.py
@@ -42,42 +42,41 @@ class Strateegery(object):
     def reinforcement(self, player, threshold = .3):
         gs = self.game_state
         Q = gs.QMap.Q
-        
-        if Q == {} and gs.step == 1:
-            print "empty dict"
-            return random.choice([i for i in range(gs.size**2) if gs.validMove(i, gs.game_sequence)]), player.mark
-        
-        t_valids = [ gs.transformIndex(index) for index in range(gs.size**2) if gs.validMove(index, gs.game_sequence) ]
+
+        valid_indices= [ index for index in range(gs.size**2) if gs.validMove(index, gs.game_sequence) ]
         
         strategies = { gs.players[0].mark : max, gs.players[1].mark : min }
                 
         player_strategy = strategies[player.mark]
 
-        charted = [ seq for seq in Q.keys() if len(seq) == gs.step]
+        charted = [ seq for seq in Q.keys() if seq[:-1] == tuple(gs.game_sequence) ]
  
-        uncharted_i = [ index for index in t_valids if index not in [ seq[-1][0] for seq in charted ] ]
+        uncharted_i = [ index for index in valid_indices if index not in [ seq[-1][0] for seq in charted ] ]
 
         # print 'game sequence: ',gs.game_sequence
+        # print 'transform seq: ',t_sequence
         # print 'transfrom seq: ',[(gs.transformIndex(m[0]), m[1] ) for m in gs.game_sequence ]
-        # print 'valids', t_valids
-        # print
-        # print 'charted', [seq[-1][0] for seq in charted]
+        print 'valids', valid_indices
+        print
+        print 'charted', [seq[-1][0] for seq in charted]
         # print
         print 'uncharted', uncharted_i
        
         if uncharted_i and random.random() > threshold and gs.step > 1:
             print 'uncharted'
-            return gs.transformIndex(random.choice(uncharted_i)), player.mark
+            return random.choice(uncharted_i), player.mark
 
-        index_and_val = { c : Q[c] for c in charted if c[-1][0] in t_valids}
+        index_and_val = { c : Q[c] for c in charted if c[-1][0] in valid_indices}
         
         if index_and_val:
             print 'optimize', player_strategy([1,9]), player.mark
             player_optimal = player_strategy(index_and_val, key=Q.get)
-            return gs.transformIndex(player_optimal[-1][0]) , player.mark
+            equivalents = [c for c in index_and_val if Q[c] == Q[player_optimal] ]
+            move_seq = random.choice(equivalents)
+            return move_seq[-1][0] , player.mark
         
         print 'just valid'        
-        return gs.transformIndex(random.choice(t_valids) ), player.mark
+        return random.choice(valid_indices) , player.mark
             
             
     ###########################################################
