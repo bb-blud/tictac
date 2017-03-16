ae186410f40167c241ca7664fe12e05322ab9fac
bb-blud <rsalo001@gmail.com>
2017-03-16 17:07:49 -0400

    feature: Begin writing kivy gui interface
    Here I am just understanding how to use the GridLayout module that
    will be the basis for the tictactoe display. There is no functionality
    actually connected to the display.

--- game_board.py
+++ game_board.py
@@ -1,30 +1,42 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-# import importlib
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty
 
-class GameBoard(Widget):
-    screen_size = (800, 600)
-    colors = {
-        'black'   : (  0,   0,   0),
-        'white'   : (255, 255, 255),
-        'red'     : (255,   0,   0),
-        'green'   : (  0, 255,   0),
-        'blue'    : (  0,   0, 255),
-        'cyan'    : (  0, 200, 200),
-        'magenta' : (200,   0, 200),
-        'yellow'  : (255, 255,   0),
-        'orange'  : (255, 128,   0)
-    }
-    pass
-    
+from utilities import *
+
+class GameBoard(GridLayout):
+    def __init__(self, game_size=3):
+        super(GameBoard, self).__init__(cols=game_size)
+        self.game_size = game_size
+        
+        for i in range(self.game_size**2):
+            self.add_widget(Button(text=str(i)))
+    # screen_size = (800, 600)
+    # colors = {
+    #     'black'   : (  0,   0,   0),
+    #     'white'   : (255, 255, 255),
+    #     'red'     : (255,   0,   0),
+    #     'green'   : (  0, 255,   0),
+    #     'blue'    : (  0,   0, 255),
+    #     'cyan'    : (  0, 200, 200),
+    #     'magenta' : (200,   0, 200),
+    #     'yellow'  : (255, 255,   0),
+    #     'orange'  : (255, 128,   0)
+    # }
+
 class TicTacApp(App):
     def build(self):
-        game = GameBoard()
+        game = GameBoard(5)
         return game
+        # size = 5
+        # layout = GridLayout(cols=size)
+        # return layout
 
 if __name__ == '__main__':
     TicTacApp().run()
-    
+
 # if __name__ == "__main__":
 #     tictacApp.run()
 
