7965b132a04e790cfb685fe8d16f60036fbf3e6c
rortms <rortms@gmail.com>
2017-03-17 01:31:46 -0400

    feature: Add mockup for selection screen
    Here I started to roughly implement a selection screen for
    the user to select which two game strategies will be pit against
    each other. As before it is just a face with no connection to the
    tictac framework. Vizually it is also very wanting at this point.

--- game_board.py
+++ game_board.py
@@ -1,18 +1,59 @@
 from kivy.app import App
+
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.properties import NumericProperty
+from kivy.uix.boxlayout import BoxLayout
 
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+from kivy.properties import ListProperty
 from utilities import *
 
+class SelectScreen(BoxLayout):
+    pass
+
+class StrategyList(BoxLayout):
+    
+    def __init__(self, **kwargs):
+        super(StrategyList, self).__init__(**kwargs)
+
+        self.strategies = ['ideal',
+                           'minimax',
+                           'Q-learning',
+                           'miniQmax',
+                           'human',
+                           'learner']
+        for strategy in self.strategies:
+            self.add_widget(Button(text=strategy))
+        
 class GameBoard(GridLayout):
+
     def __init__(self, game_size=3):
         super(GameBoard, self).__init__(cols=game_size)
         self.game_size = game_size
         
         for i in range(self.game_size**2):
             self.add_widget(Button(text=str(i)))
+
+class TicTacApp(App):
+    
+    def build(self):
+        game = SelectScreen()#GameBoard(5)
+        return game
+
+if __name__ == '__main__':
+    TicTacApp().run()
+
+
+    # layout = BoxLayout(orientation='vertical')
+    # btn1 = Label(text='Hello')
+    # btn2 = Label(text='World')
+    # layout.add_widget(btn1)
+    # layout.add_widget(btn2)
+    # return layout
+
+
     # screen_size = (800, 600)
     # colors = {
     #     'black'   : (  0,   0,   0),
@@ -25,48 +66,3 @@ class GameBoard(GridLayout):
     #     'yellow'  : (255, 255,   0),
     #     'orange'  : (255, 128,   0)
     # }
-
-class TicTacApp(App):
-    def build(self):
-        game = GameBoard(5)
-        return game
-        # size = 5
-        # layout = GridLayout(cols=size)
-        # return layout
-
-if __name__ == '__main__':
-    TicTacApp().run()
-
-# if __name__ == "__main__":
-#     tictacApp.run()
-
-    # def __init__(self, display=True):
-    #     self.display = display
-    #     self.done = False
-
-    #     if self.display:
-    #         try:
-    #             self.pygame = importlib.import_module('pygame')
-    #             self.pygame.init()
-    #             self.screen = self.pygame.display.set_mode(self.screen_size)
-    #         except ImportError as e:
-    #             self.display = False
-    #             print "Simulator.__init__(): Unable to import pygame; display disabled.\n{}: {}".format(e.__class__.__name__, e)
-    #         except Exception as e:
-    #             self.display = False
-    #             print "Simulator.__init__(): Error initializing GUI objects; display disabled.\n{}: {}".format(e.__class__.__name__, e)
-
-    # def run(self):
-    #     while not self.done:
-    #         try:
-    #             for event in self.pygame.event.get():
-    #                 if event.type == self.pygame.QUIT:
-    #                     self.done = True
-    #             self.pygame.display.flip()
-                
-    #         except Exception as e:
-    #             print "WTF: {}".format(e.__class__.__name__,e)
-                                      
-           
-                
-        
